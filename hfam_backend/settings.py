663885b8adb49617807270c1b7bf84cdf4ec3c99
htouloupas <harrytouloupas@gmail.com>
2020-04-04 14:25:24 +0300

    added enabled cors for every host

--- hfam_backend/settings.py
+++ hfam_backend/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'apps.hfamAPI',
     'apps.simulation',
     'drf_yasg',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,6 +58,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'hfam_backend.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 TEMPLATES = [
     {
